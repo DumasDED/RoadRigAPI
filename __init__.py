951b7e29366b5184e0c129572f23dee6a556fa61
Darrell <Dumas.DED@gmail.com>
2018-02-11 08:19:56 -0500

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,32 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+import database as db
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Bands(Resource):
+    def get(self):
+        return list(db.get_all_nodes('band'))
+
+
+class Band(Resource):
+    def get(self, username):
+        return db.get_node('band', 'username', username)
+
+
+class Band_Events(Resource):
+    def get(self, username):
+        return db.get
+
+
+api.add_resource(Bands, '/bands')
+api.add_resource(Band, '/bands/<username>')
+# api.add_resource(Venues, '/venues')
+# api.add_resource(Events, '/events')
+
+
+if __name__ == '__main__':
+     app.run(port=5002)
---
+++ database.py
@@ -0,0 +1,46 @@
+from py2neo import Graph, Node, Relationship
+
+import config
+import error
+
+try:
+    db = Graph(password=config.db_password)
+except error.types as e:
+    error.handle(e)
+
+
+def check_node(label, key, value):
+    node = db.find_one(label, key, value)
+    return node is not None
+
+
+def add_node(label, **properties):
+    node = Node(label, **properties)
+    return db.create(node)
+
+
+def get_node(label, key, value):
+    node = db.find_one(label, key, value)
+    return node
+
+
+def get_all_nodes(label):
+    nodes = db.find(label)
+    return nodes
+
+
+def check_relationship(node1, relationship_type, node2=None):
+    rel = db.match_one(start_node=node1, rel_type=relationship_type, end_node=node2)
+    return rel is not None
+
+
+def add_relationship(node1, relationship_type, node2):
+    rel = Relationship(node1, relationship_type, node2)
+    rtn = db.create(rel)
+    return rtn
+
+
+def get_relationship(node1, relationship_type, node2):
+    rel = db.match_one(node1, relationship_type, node2)
+    return rel,
+

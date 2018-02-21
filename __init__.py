a7724572292a3abe3b34147c70f40b9aedfb1d4e
Darrell <Dumas.DED@gmail.com>
2018-02-20 22:01:15 -0500

    Band events service, mods to db access

--- __init__.py
+++ __init__.py
@@ -19,14 +19,15 @@ class Band(Resource):
 
 class Band_Events(Resource):
     def get(self, username):
-        return db.get
+        return db.get_related_nodes('band', 'username', username, 'event')
 
 
 api.add_resource(Bands, '/bands')
 api.add_resource(Band, '/bands/<username>')
+api.add_resource(Band_Events, '/bands/<username>/events')
 # api.add_resource(Venues, '/venues')
 # api.add_resource(Events, '/events')
 
 
 if __name__ == '__main__':
-     app.run(port=5002)
+    app.run(port=5002)
--- database.py
+++ database.py
@@ -9,38 +9,17 @@ except error.types as e:
     error.handle(e)
 
 
-def check_node(label, key, value):
-    node = db.find_one(label, key, value)
-    return node is not None
-
-
-def add_node(label, **properties):
-    node = Node(label, **properties)
-    return db.create(node)
-
-
 def get_node(label, key, value):
     node = db.find_one(label, key, value)
     return node
 
 
-def get_all_nodes(label):
-    nodes = db.find(label)
+def get_related_nodes(parent_label, key, value, child_label):
+    nodes = db.data("match (a:%s)-[]-(b:%s) where a.%s = '%s' return b" % (parent_label, child_label, key, value))
+    nodes = [node['b'] for node in nodes]
     return nodes
 
 
-def check_relationship(node1, relationship_type, node2=None):
-    rel = db.match_one(start_node=node1, rel_type=relationship_type, end_node=node2)
-    return rel is not None
-
-
-def add_relationship(node1, relationship_type, node2):
-    rel = Relationship(node1, relationship_type, node2)
-    rtn = db.create(rel)
-    return rtn
-
-
-def get_relationship(node1, relationship_type, node2):
-    rel = db.match_one(node1, relationship_type, node2)
-    return rel,
-
+def get_all_nodes(label):
+    nodes = db.find(label)
+    return nodes

e69767c67195a06fa63b8cf01dafa72f3b90de3d
Darrell <Dumas.DED@gmail.com>
2018-02-21 06:56:08 -0500

    city & city venue endpoints

--- __init__.py
+++ __init__.py
@@ -22,9 +22,27 @@ class Band_Events(Resource):
         return db.get_related_nodes('band', 'username', username, 'event')
 
 
+class Cities(Resource):
+    def get(self):
+        return list(db.get_all_nodes('city'))
+
+
+class City(Resource):
+    def get(self, name):
+        return db.get_node('city', 'name', name)
+
+
+class City_Venues(Resource):
+    def get(self, name):
+        return db.get_related_nodes('city', 'name', name, 'venue')
+
+
 api.add_resource(Bands, '/bands')
 api.add_resource(Band, '/bands/<username>')
 api.add_resource(Band_Events, '/bands/<username>/events')
+api.add_resource(Cities, '/cities')
+api.add_resource(City, '/cities/<name>')
+api.add_resource(City_Venues, '/cities/<name>/venues')
 # api.add_resource(Venues, '/venues')
 # api.add_resource(Events, '/events')
 

44effab9ee385f99722f811e2aa6f4d802ee8872
Darrell <Dumas.DED@gmail.com>
2018-03-07 09:55:53 -0500

    City state list method

--- __init__.py
+++ __init__.py
@@ -17,9 +17,14 @@ class Band(Resource):
         return db.get_node('band', 'username', username)
 
 
-class Band_Events(Resource):
+class BandEvents(Resource):
     def get(self, username):
-        return db.get_related_nodes('band', 'username', username, 'event')
+        return db.get_child_nodes('band', 'username', username, 'event')
+
+
+class CityStateList(Resource):
+    def get(self):
+        return db.get_related_nodes('city', 'state')
 
 
 class Cities(Resource):
@@ -32,19 +37,54 @@ class City(Resource):
         return db.get_node('city', 'name', name)
 
 
-class City_Venues(Resource):
+class CityVenues(Resource):
     def get(self, name):
-        return db.get_related_nodes('city', 'name', name, 'venue')
+        return db.get_child_nodes('city', 'name', name, 'venue')
+
+
+class Venues(Resource):
+    def get(self):
+        return db.get_all_nodes('venue')
+
+
+class Venue(Resource):
+    def get(self, username):
+        return db.get_node('venue', 'username', username)
+
+
+class VenueEvents(Resource):
+    def get(self, username):
+        return db.get_child_nodes('venue', 'username', username, 'event')
+
+
+class Event(Resource):
+    def get(self, id):
+        return db.get_node('event', 'id', id)
+
+
+class EventBands(Resource):
+    def get(self, id):
+        return db.get_child_nodes('event', 'id', id, 'band')
+
+
+class EventVenue(Resource):
+    def get(self, id):
+        return db.get_child_nodes('event', 'id', id, 'venue')
 
 
 api.add_resource(Bands, '/bands')
 api.add_resource(Band, '/bands/<username>')
-api.add_resource(Band_Events, '/bands/<username>/events')
+api.add_resource(BandEvents, '/bands/<username>/events')
+api.add_resource(CityStateList, '/cityStateList')
 api.add_resource(Cities, '/cities')
 api.add_resource(City, '/cities/<name>')
-api.add_resource(City_Venues, '/cities/<name>/venues')
-# api.add_resource(Venues, '/venues')
-# api.add_resource(Events, '/events')
+api.add_resource(CityVenues, '/cities/<name>/venues')
+api.add_resource(Venues, '/venues')
+api.add_resource(Venue, '/venues/<username>')
+api.add_resource(VenueEvents, '/venues/<username>/events')
+api.add_resource(Event, '/events/<id>')
+api.add_resource(EventVenue, '/events/<id>/venue')
+api.add_resource(EventBands, '/events/<id>/bands')
 
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -14,7 +14,7 @@ def get_node(label, key, value):
     return node
 
 
-def get_related_nodes(parent_label, key, value, child_label):
+def get_child_nodes(parent_label, key, value, child_label):
     nodes = db.data("match (a:%s)-[]-(b:%s) where a.%s = '%s' return b" % (parent_label, child_label, key, value))
     nodes = [node['b'] for node in nodes]
     return nodes
@@ -22,4 +22,9 @@ def get_related_nodes(parent_label, key, value, child_label):
 
 def get_all_nodes(label):
     nodes = db.find(label)
+    return nodes
+
+
+def get_related_nodes(left_label, right_label):
+    nodes = db.data("match ({0}:{0})-[]-({1}:{1}) return {0}, {1}".format(left_label, right_label))
     return nodes

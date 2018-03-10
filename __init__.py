0d6e543fc224d724822ed779317047450cd41fc5
Darrell <Dumas.DED@gmail.com>
2018-03-10 12:42:17 -0500

    updated service requests, initial direct DB access

--- __init__.py
+++ __init__.py
@@ -1,10 +1,23 @@
-from flask import Flask, request
+from flask import Flask
+from flask_cors import CORS
 from flask_restful import Resource, Api
 
+# TODO: get rid of database, incorporate all calls directly
 import database as db
 
+from py2neo import Graph, Node, Relationship
+
+import config
+import error
+
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
+
+try:
+    db_direct = Graph(password=config.db_password)
+except error.types as e:
+    error.handle(e)
 
 
 class Bands(Resource):
@@ -39,7 +52,13 @@ class City(Resource):
 
 class CityVenues(Resource):
     def get(self, name):
-        return db.get_child_nodes('city', 'name', name, 'venue')
+        nodes = db_direct.data("""
+            match (b:band)-[]-(e:event)-[]-(n:venue)-[]-(c:city)
+            where c.name = '%s'
+            return n, count(e) as events, count(distinct b) as bands
+            order by count(e) desc""" % name)
+        nodes = [n['n'] for n in nodes]
+        return nodes
 
 
 class Venues(Resource):
@@ -53,8 +72,21 @@ class Venue(Resource):
 
 
 class VenueEvents(Resource):
-    def get(self, username):
-        return db.get_child_nodes('venue', 'username', username, 'event')
+    def get(self, identifier):
+        nodes = db_direct.data("""
+                    match (e:event)-[]-(v:venue)
+                    where v.%s= '%s'
+                    return e""" % (search_by(identifier), identifier))
+        return nodes
+
+
+class VenueBands(Resource):
+    def get(self, identifier):
+        nodes = db_direct.data("""
+            match (b:band)-[]-(e:event)-[]-(v:venue)
+            where v.%s= '%s'
+            return b""" % (search_by(identifier), identifier))
+        return nodes
 
 
 class Event(Resource):
@@ -72,6 +104,13 @@ class EventVenue(Resource):
         return db.get_child_nodes('event', 'id', id, 'venue')
 
 
+def search_by(searchvalue):
+    try:
+        float(searchvalue)
+        return 'id'
+    except ValueError:
+        return 'username'
+
 api.add_resource(Bands, '/bands')
 api.add_resource(Band, '/bands/<username>')
 api.add_resource(BandEvents, '/bands/<username>/events')
@@ -81,7 +120,8 @@ api.add_resource(City, '/cities/<name>')
 api.add_resource(CityVenues, '/cities/<name>/venues')
 api.add_resource(Venues, '/venues')
 api.add_resource(Venue, '/venues/<username>')
-api.add_resource(VenueEvents, '/venues/<username>/events')
+api.add_resource(VenueEvents, '/venues/<identifier>/events')
+api.add_resource(VenueBands, '/venues/<identifier>/bands')
 api.add_resource(Event, '/events/<id>')
 api.add_resource(EventVenue, '/events/<id>/venue')
 api.add_resource(EventBands, '/events/<id>/bands')
